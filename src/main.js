d7231c81e088596a35eae663252989d9b6f73d7c
jessie <1143994606@qq.com>
2020-03-15 20:54:26 +0800

    rem适配（需要两个插件）

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant组件库
 import 'vant/lib/index.css' // 引入vant组件的样式
+import 'amfe-flexible'
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 // 以上做法是完整导入，在项目开发阶段使用，配置方便节省时间，将来上线的时候可以使用按需导入。
 Vue.config.productionTip = false
