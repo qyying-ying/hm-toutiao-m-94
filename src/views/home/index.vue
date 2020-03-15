60814b34484a37907001a1eb329b2d7ed6e35baa
jessie <1143994606@qq.com>
2020-03-15 20:13:58 +0800

    全局注册使用vant

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
-    首页
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Home',
-  components: {
-
-  }
+  components: {}
 }
 </script>
