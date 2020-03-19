bab5d5902fc8f79d8a10fcb0aaef220395194098
jessie <1143994606@qq.com>
2020-03-19 23:51:02 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -5,12 +5,16 @@
 <div class="scroll-wrapper">
     <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
        绑定的 loading 变成true  -->
-    <van-list finished-text="have no" v-model="upLoading" :finished="finished" @load="onLoad">
+       <!-- 下拉刷新组件包裹 列表组件 -->
+    <van-pull-refresh v-model=" downLoading" @refresh="onRefresh" :success-text="successText">
+      <van-list finished-text="have no" v-model="upLoading" :finished="finished" @load="onLoad">
         <!-- 循环内容 -->
-        <van-cell-group>
-          <van-cell v-for="item in articles" :key="item" title="jiyou" :value="'china' + item"></van-cell>
-        </van-cell-group>
-    </van-list>
+         <van-cell-group>
+           <van-cell v-for="item in articles" :key="item" title="jiyou" :value="'china' + item"></van-cell>
+         </van-cell-group>
+      </van-list>
+    </van-pull-refresh>
+
 </div>
 </template>
 
@@ -18,12 +22,15 @@
 export default {
   data () {
     return {
+      successText: '',
+      downLoading: false,
       upLoading: false,
       finished: false,
       articles: [] // 文章列表
     }
   },
   methods: {
+    // 上拉加载
     onLoad () {
       console.log('start loading')
       if (this.articles.length > 50) {
@@ -35,10 +42,20 @@ export default {
         // 加载完数据 需要手动关掉loading
         this.upLoading = false
       }
-
     //   setTimeout(() => {
     //     this.finished = true // 表示数据已经全部加载完毕 没有数据了
     //   }, 1000)
+    },
+    // 下拉刷新
+    onRefresh () {
+      setTimeout(() => {
+        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        // 数组添加到头部
+        this.articles.unshift(...arr)
+        // 手动关闭
+        this.downLoading = false
+        this.successText = `更新了${arr.length}条数据`
+      }, 1000)
     }
   }
 }
