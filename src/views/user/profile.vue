702e625e24b28d4e9a095d67ffe7a19d7f11373d
jessie <1143994606@qq.com>
2020-04-02 20:47:00 +0800

    个人中心-编辑资料-基础布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,15 +1,66 @@
 <template>
   <div class="container">
-    <!-- 导航 显示返回箭头 -->
-    <!-- click-left点击左侧事件 -->
-    <!-- $router.go(-1) $router.back()都是返回上一页 -->
-    <van-nav-bar title="编辑资料" right-text="保存" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像"  center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell is-link title="名称" value="用户名称" />
+      <van-cell is-link title="性别" value='男'/>
+      <van-cell is-link title="生日" value="2019-08-08" />
+    </van-cell-group>
+     <!-- 放置 头像弹层 -->
+      <!-- 弹层组件 -->
+    <van-popup v-model="showPhoto" style="width:80%">
+      <!-- 内容 -->
+      <!-- 1 本地相册选择图片 -->
+      <!-- 2 拍照 -->
+       <van-cell is-link title="本地相册选择图片"></van-cell>
+       <van-cell is-link title="拍照"></van-cell>
+    </van-popup>
+    <!-- 放置昵称弹层 -->
+    <!-- 弹昵称 -->
+    <van-popup v-model="showName" style="width:80%">
+       <!-- 编辑用户昵称  双向绑定用户的昵称-->
+       <van-field  type='textarea'  rows="4"></van-field>
+    </van-popup>
+    <!-- 性别弹层 -->
+    <van-action-sheet :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
+    <!-- 生日弹层 -->
+    <van-popup v-model="showBirthDay" position="bottom">
+      <!-- 选择出生日期  出生日期应该小于现在时间-->
+      <!-- type表示 当前的日期类型 年月日 -->
+      <van-datetime-picker
+           v-model="currentDate"
+           type="date"
+          :min-date="minDate"
+          :max-date="maxDate"
+         />
+    </van-popup>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      showPhoto: false, // 显示头像弹层
+      showName: false,
+      showGender: false,
+      showBirthDay: false,
+      minDate: new Date(1900, 1, 1), // 生日组件 选择的最小日期
+      maxDate: new Date(),
+      currentDate: new Date(), // 当弹出层是 显示的时间 是今天
+      actions: [{ name: '男' }, { name: '女' }]
+    }
+  }
 }
 </script>
 
