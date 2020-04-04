e5e33b4e2d149bed61981189c682b517f47e8cb0
jessie <1143994606@qq.com>
2020-04-04 19:01:32 +0800

    打包完成

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,16 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    // config参数就是当前vue-cli项目所有的webpack配置
+    // 如果想要改动 直接修改这个信息就可以
+    // 首先判断当前的打包环境是 开发环境还是生产环境
+    if (process.env.NODE_ENV === 'production') {
+      // 只需要在生产环境中 把所有的console删除掉并且再打包 这里的删除console不是删除源代码的console 而是删除打包出来的console
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      // 这个东西 是根据 webpack的配置得来的
+    }
+  },
+  publicPath: './' // 打包后的引用地址
 }
