8fc79c890664285e90c52370db95d3a1777969fd
jessie <1143994606@qq.com>
2020-04-04 18:12:45 +0800

    组件缓存升级

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <!-- 一级路由容器 -->
-    <router-view />
+    <!-- 如果不加控制 会缓存所有的组件实例 -->
+    <!-- 只要keep-alive包裹 就会缓存所有实例 -->
+    <!-- 如果不包裹 就不缓存实例 -->
+    <keep-alive>
+      <router-view v-if="$route.meta.isAlive" />
+    </keep-alive>
+    <!-- 条件渲染 -->
+    <router-view v-if="!$route.meta.isAlive"></router-view>
+     <!-- 要根据当前的meta中的isAlive属性 决定是否缓存组件 $route.neta.isAlive  -->
   </div>
 </template>
 
